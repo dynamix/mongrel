6543eda672fc648d52aee49c9cb56e09508dfc89
frostbyte <frostbyte@roadrunner.com>
2010-08-09 11:00:40 -0700

    Add 400 response boilerplate.

--- lib/mongrel/const.rb
+++ lib/mongrel/const.rb
@@ -1,4 +1,3 @@
-
 module Mongrel
 
   # Every standard HTTP code mapped to the appropriate message.  These are
@@ -68,6 +67,9 @@ module Mongrel
     MONGREL_VERSION="1.1.5".freeze
 
     MONGREL_TMP_BASE="mongrel".freeze
+
+    # A standard 400 response for a request which generates a http parse exception
+    ERROR_400_RESPONSE="HTTP/1.1 400 Bad Request\r\nConnection: Close\r\nServer: Mongrel #{MONGREL_VERSION}\r\n\r\nBAD REQUEST".freeze
 
     # The standard empty 404 response for bad requests.  Use Error4040Handler for custom stuff.
     ERROR_404_RESPONSE="HTTP/1.1 404 Not Found\r\nConnection: close\r\nServer: Mongrel #{MONGREL_VERSION}\r\n\r\nNOT FOUND".freeze
